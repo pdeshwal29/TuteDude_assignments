a151b0dd50a47d7a208638f6ed2fbf216ef94e76
pdeshwal29 <deshwal.preeti29@gmail.com>
2025-11-30 13:41:28 +0530

    initial commit

---
+++ tutedude_test.py
@@ -0,0 +1,15 @@
+"""
+print("i am no one")
+print("hello")
+print(10)
+
+x=int(input("enter a number: "))
+y=int(input("enter another number: "))
+result=x+y
+print("the addition of these are: ", result)
+"""
+
+b=float(input("enter the base of the triangle: "))
+h=float(input("enter height of triangle: "))
+area=0.5*b*h
+print("area of triangle is: ", area)
