ed927eb6aaab6be1b6caba8a5375f9367c7254f3
pdeshwal29 <deshwal.preeti29@gmail.com>
2025-11-30 15:58:51 +0530

    adding assignments

---
+++ Basic_Mathematical_Operations.py
@@ -0,0 +1,11 @@
+x=float(input("enter a number x: "))
+y=float(input("enter a number y: "))
+add=x+y
+sub=x-y
+div=x/y
+mult=x*y
+
+print("addition of x and y: ", add)
+print("subtraction of x and y: ", sub)
+print("division of x and y: ", div)
+print("multiplication of x and y: ", mult)
---
+++ Create_Personalized_Greeting.py
@@ -0,0 +1,4 @@
+first=input("enter first name: ")
+last=input("enter last name: ")
+
+print("Hello ", first, "", last, "! Welcome to the Python program")
